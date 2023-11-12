6aa82fa537bcff911b5e0b0f2f8e8d33a97cfac9
Kartik Khorwal <hellokartikk@gmail.com>
2023-11-12 23:30:35 +0530

    update user

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getUsers, deleteUserById } from '../db/users';
+import { getUsers, deleteUserById, getUserById } from '../db/users';
 
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
@@ -25,6 +25,30 @@ export const deleteUser = async (req: express.Request, res: express.Response) =>
         const deletedUser = await deleteUserById(id);
 
         return res.json(deletedUser).status(200).end();
+
+    } catch (error) {
+        return res.send({ error, message: 'users not deleted' }).status(400).end();
+    }
+
+}
+
+
+
+export const updateUser = async (req: express.Request, res: express.Response) => {
+
+    try {
+
+        const { id } = req.params;
+        const { username } = req.body;
+        if (!username) return res.send({ message: 'username is required' }).status(400).end();
+
+        const user = await getUserById(id);
+
+        if (user.username === username) return res.send({ message: 'username cannot be same' }).status(400).end();
+        user.username = username;
+        await user.save();
+
+        return res.json(user).status(200).end();
 
     } catch (error) {
         return res.send({ error, message: 'users not deleted' }).status(400).end();
